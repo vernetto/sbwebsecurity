f815e35b863768a33360cc186c3f1dd8af5f7329
vernetto <publicpierre@gmail.com>
2020-04-26 10:24:13 +0200

    first

---
+++ src/test/java/org/pierre/sbwebsecurity/SbwebsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pierre.sbwebsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbwebsecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
